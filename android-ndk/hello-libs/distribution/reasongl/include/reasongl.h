61dd1873fd6e3f0b715e1c2137246eb87d2abe5a
Jared Forsyth <jared@jaredforsyth.com>
2017-12-05 09:50:24 -0700

    using this example

---
+++ android-ndk/hello-libs/distribution/reasongl/include/reasongl.h
@@ -0,0 +1,16 @@
+
+#ifndef __REASONGL_HPP__
+#define __REASONGL_HPP__
+
+#include <sys/types.h>
+
+/*
+ * return current system ticks
+ */
+#ifdef __cplusplus
+extern "C"
+#endif // __cplusplus
+void caml_startup(char**);
+
+#endif //__REASONGL_HPP__
+
